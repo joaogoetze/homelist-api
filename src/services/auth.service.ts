46b74869eac77c7678bfad9daad0fffc25373a9b
joaogoetze <joaoaugustoferreira5@gmail.com>
2026-07-18 13:38:40 -0300

    logs

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -27,6 +27,7 @@ export class AuthService {
     }
 
     async login(email: string, password: string) {
+        console.info("login", email, password);
         const user = await this.authRepository.getUserByEmail(email);
 
         if (!user) {
